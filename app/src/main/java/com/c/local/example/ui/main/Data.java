441d204fd5911eb612fcac8ec6df2ee07136dda1
山尾 和義 <kyamao@opendoor.co.jp>
2021-05-11 16:21:25 +0900

    レイアウトとバインディング式

---
+++ app/src/main/java/com/c/local/example/ui/main/Data.java
@@ -0,0 +1,18 @@
+package com.c.local.example.ui.main;
+
+public class Data {
+
+	private String text;
+
+	Data() {
+		this.text = "init";
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
--- app/src/main/java/com/c/local/example/ui/main/MainFragment.java
+++ app/src/main/java/com/c/local/example/ui/main/MainFragment.java
@@ -10,13 +10,10 @@ import com.c.local.example.databinding.MainFragmentBinding;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 public class MainFragment extends Fragment {
 
-	private MainViewModel mViewModel;
 	private MainFragmentBinding binding;
-
 
 	public static MainFragment newInstance() {
 		return new MainFragment();
@@ -26,34 +23,20 @@ public class MainFragment extends Fragment {
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
 							 @Nullable Bundle savedInstanceState) {
-
 		binding = MainFragmentBinding.inflate(inflater, container, false);
 		View view = binding.getRoot();
 		return view;
-		// return inflater.inflate(R.layout.main_fragment, container, false);
 	}
 
 	@Override
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-		binding.message.setText("Click");
+		// ビュー バインディング
+		binding.setData(new Data());
 		binding.button.setOnClickListener(v -> {
 			binding.text.setText("time: " + System.currentTimeMillis());
 		});
-
-
-		// binding.setVm(mViewModel);
-//		binding.button.setOnClickListener(new View.OnClickListener() {
-//        viewModel.userClicked()
-//		});
-
-
-//		ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.main_fragment);
-//		binding.vm = mViewModel;
-
-		// TODO: Use the ViewModel
 	}
 
 }
