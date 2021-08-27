898ca70a5e2e1964b5416d6c7edb6d776568994a
山尾 和義 <kyamao@opendoor.co.jp>
2021-08-27 19:41:18 +0900

    test

--- app/src/main/java/c/local/com/example/viewmodel/HiltTestViewModel.java
+++ app/src/main/java/c/local/com/example/viewmodel/HiltTestViewModel.java
@@ -2,27 +2,21 @@ package c.local.com.example.viewmodel;
 
 import java.util.List;
 
+import androidx.hilt.lifecycle.ViewModelInject;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import c.local.com.example.Repository;
 import c.local.com.example.data.Pokemon;
 
 public class HiltTestViewModel extends ViewModel {
 	public static final String TAG = HiltTestViewModel.class.getSimpleName();
 
+	private Repository repository;
 
 	private MutableLiveData<List<Pokemon>> pokemonList = new MutableLiveData<>();
 
-	public HiltTestViewModel() {
-
+	@ViewModelInject
+	public HiltTestViewModel(Repository repository) {
+		this.repository = repository;
 	}
-
-	public MutableLiveData<List<Pokemon>> getPokemonList() {
-		return pokemonList;
-	}
-
-	public void getPokemons() {
-
-	}
-
-
 }
