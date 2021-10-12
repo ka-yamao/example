556240d5b77a923a9d40db315d3284c828c585d6
山尾 和義 <kyamao@opendoor.co.jp>
2021-10-12 20:18:41 +0900

    スライダー、グラフの追加

---
+++ app/src/main/java/com/c/local/myapplication/ui/main/MainFragment.java
@@ -0,0 +1,29 @@
+package com.c.local.myapplication.ui.main;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.c.local.myapplication.R;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+	private MainViewModel mViewModel;
+
+	public static MainFragment newInstance() {
+		return new MainFragment();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+							 @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.main_fragment, container, false);
+	}
+
+}
---
+++ app/src/main/java/com/c/local/myapplication/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.c.local.myapplication.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+	// TODO: Implement the ViewModel
+}
